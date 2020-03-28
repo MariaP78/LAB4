e1d91d7cdb7e301f914cc1d80d180084e90d3e1c
Monica <61684336+monix8400@users.noreply.github.com>
2020-03-29 01:49:29 +0200

    controller

--- LAB4/ApothekeController.cpp
+++ LAB4/ApothekeController.cpp
@@ -1,1 +1,10 @@
 #include "ApothekeController.h"
+
+ApothekeController::ApothekeController() {
+
+}
+
+void ApothekeController::meniu()
+{
+
+}
--- LAB4/ApothekeController.h
+++ LAB4/ApothekeController.h
@@ -1,5 +1,12 @@
 #pragma once
+#include "ApothekeRepo.h"
+#include <iostream>
+#include <string>
+
 class ApothekeController
 {
+public:
+	ApothekeController();
+	void meniu();
 };
 
